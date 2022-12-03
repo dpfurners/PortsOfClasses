359f63ae4f06ae8617439239e0ea7cbd9f89f55c
Daniel Pfurner <dpfurner@tsn.at>
2022-12-03 17:14:23 +0100

    Make ContractBase available from models module

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
 from .company import Company
 from .ships import *
 from .harbor import HarborBase
+from .contract import ContractBase
 
 __all__ = [
     "Company",
     "ShipBase",
-    "HarborBase"
+    "HarborBase",
+    "ContractBase"
 ]
