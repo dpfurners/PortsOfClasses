d792d779d3a5d0b53e82c2e0452552455ca42a22
Daniel Pfurner <dpfurner@tsn.at>
2022-12-03 20:20:30 +0100

    Display bigger amounts of money, Make fields and class attribute

--- ui/ships.py
+++ ui/ships.py
@@ -21,10 +21,11 @@ class ShipShop(Screen):
         self.ships = ships
         self.back_action = None
 
+        self.fields: dict = {}
+
         self.inspect: ShipBase | None = None
 
     def display_available_ships(self):
-        fields = {}
         if len(self.ships) <= 10:
             for index, ship in enumerate(self.ships):
                 x = y = 0
@@ -36,15 +37,13 @@ class ShipShop(Screen):
 
                 draw_text(f"Capacity: {ship.capacity:_}", FONT_DARK, self.screen, x+5, y+50, 30)
                 draw_text(f"Price: {ship.price:_}$", FONT_DARK, self.screen, x+5, y+72.5, 35)
-                fields[ship] = [ship, rect]
-        return fields
+                self.fields[ship] = [ship, rect]
 
     def display_ship(self, ship):
-        fields = {}
         pygame.draw.rect(self.screen, (0, 161, 255), pygame.Rect(75, 125, 845, 520), 0, 3)
 
         back = new_button(self.screen, (864, 65), (50, 50), picture="./resources/textures/cross.png")
-        fields["back"] = [0, back]
+        self.fields["back"] = [0, back]
 
         draw_text(ship.name, FONT_DARK, self.screen, 80, 130, 60)
         draw_text(ship.type, FONT_DARK, self.screen, 85, 170, 40)
@@ -55,33 +54,32 @@ class ShipShop(Screen):
         draw_text(f"Price: {ship.price:_}$", FONT_DARK, self.screen, 85, 610, 40)
 
         buy = new_button(self.screen, (720, 450), (200, 200), picture="./resources/textures/buy.png")
-        fields["buy"] = [0, buy]
-        return fields
+        self.fields["buy"] = [0, buy]
 
     def startup_screen(self):
         run = True
-        fields = {}
         click = False
         while run:
+            self.fields = {}
             self.screen.blit(self.bg, (0, 0))
             pygame.draw.rect(self.screen, (255, 255, 255), pygame.Rect(50, 50, 900, 600), 0, 5)
 
             draw_text("Boat-Sales", FONT_DARK, self.screen, 75, 75, 50)
-            draw_text(f"Balance: {self.company.get_current_money:_}$", FONT_DARK, self.screen, 500, 75, 50)
+            draw_text(f"Balance: {self.company.get_current_money:_}$", FONT_DARK, self.screen, 500, 75, 40)
 
             if isinstance(self.inspect, ShipBase):
-                fields = self.display_ship(self.inspect)
+                self.display_ship(self.inspect)
             else:
-                fields = self.display_available_ships()
+                self.display_available_ships()
 
             if self.company.ships:
                 go_on = new_button(self.screen, (864, 65), (50, 50), picture="./resources/textures/cross.png")
-                fields["go_on"] = [0, go_on]
+                self.fields["go_on"] = [0, go_on]
 
             mx, my = pygame.mouse.get_pos()
             # print(fields)
-            for field in fields:
-                if fields[field][1].collidepoint((mx, my)):
+            for field in self.fields:
+                if self.fields[field][1].collidepoint((mx, my)):
                     if click:
                         if field == "back":
                             self.inspect = None
@@ -100,7 +98,7 @@ class ShipShop(Screen):
                         elif field == "go_on":
                             self.back_action()
                         else:
-                            self.inspect = fields[field][0]
+                            self.inspect = self.fields[field][0]
                             pygame.time.wait(100)
 
             for event in pygame.event.get():
@@ -124,10 +122,13 @@ class ShipDepot(Screen):
         self.back_action = None
         self.select: bool = False
 
+        self.fields: dict = {}
+
         self.inspect: ShipBase | None = None
 
     def display_owned_ships(self):
-        fields = {}
+        go_on = new_button(self.screen, (864, 65), (50, 50), picture="./resources/textures/cross.png")
+        self.fields["go_on"] = [0, go_on]
         if len(self.company.ships) <= 10:
             for index, ship in enumerate(self.company.ships):
                 x = y = 0
@@ -145,15 +146,13 @@ class ShipDepot(Screen):
                         draw_text("Done", FONT_DARK, self.screen, x + 300, y + 30, 60)
                     else:
                         draw_text(ship.contract.strfdelta(), FONT_DARK, self.screen, x+300, y+30, 60)
-                fields[ship] = [ship, rect]
-        return fields
+                self.fields[ship] = [ship, rect]
 
     def display_ship(self, ship: ShipBase):
-        fields = {}
         pygame.draw.rect(self.screen, (0, 161, 255), pygame.Rect(75, 125, 845, 520), 0, 3)
 
         back = new_button(self.screen, (864, 65), (50, 50), picture="./resources/textures/cross.png")
-        fields["back"] = [0, back]
+        self.fields["back"] = [0, back]
 
         draw_text(ship.name, FONT_DARK, self.screen, 80, 130, 60)
         draw_text(ship.type, FONT_DARK, self.screen, 85, 170, 40)
@@ -170,36 +169,29 @@ class ShipDepot(Screen):
 
         if self.select:
             select = new_button(self.screen, (720, 550), (200, 100), picture="./resources/textures/arrow.png")
-            fields["select"] = [0, select]
-        return fields
+            self.fields["select"] = [0, select]
 
     def startup_screen(self, select: bool = False):
         self.select = select
         run = True
-        fields = {}
+
         click = False
         while run:
+            self.fields = {}
             self.screen.blit(self.bg, (0, 0))
             pygame.draw.rect(self.screen, (255, 255, 255), pygame.Rect(50, 50, 900, 600), 0, 5)
 
             draw_text("Boat-Sales", FONT_DARK, self.screen, 75, 75, 50)
-            draw_text(f"Balance: {self.company.get_current_money:_}$", FONT_DARK, self.screen, 500, 75, 50)
+            draw_text(f"Balance: {self.company.get_current_money:_}$", FONT_DARK, self.screen, 500, 75, 40)
 
             if isinstance(self.inspect, ShipBase):
-                fields = self.display_ship(self.inspect)
+                self.display_ship(self.inspect)
             else:
-                fields = self.display_owned_ships()
-
-            if self.company.ships:
-                go_on = pygame.Rect(864, 65, 50, 50)
-                self.screen.blit(pygame.image.load("./resources/textures/cross.png"), (864, 65))
-                # pygame.draw.rect(self.screen, (255, 161, 0), go_on, 0, 2)
-                fields["go_on"] = [0, go_on]
+                self.display_owned_ships()
 
             mx, my = pygame.mouse.get_pos()
-            # print(fields)
-            for field in fields:
-                if fields[field][1].collidepoint((mx, my)):
+            for field in self.fields:
+                if self.fields[field][1].collidepoint((mx, my)):
                     if click:
                         if field == "back":
                             self.inspect = None
@@ -211,7 +203,7 @@ class ShipDepot(Screen):
                         elif field == "go_on":
                             self.back_action()
                         else:
-                            self.inspect = fields[field][0]
+                            self.inspect = self.fields[field][0]
                             pygame.time.wait(100)
 
             for event in pygame.event.get():
